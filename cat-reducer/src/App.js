b23e8e081b8ace4cc3e4f87a967579f9bddc7617
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-11-20 14:36:33 -0500

    in class walk through of context api done.

--- cat-reducer/src/App.js
+++ cat-reducer/src/App.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
-import Activity from "./components/activity"
+import Activity from "./components/activity";
+import { StateProvider } from "./context";
 
-function App() {
-  return (
-    <div className="App">
-      <Activity/>
-    </div>
-  )
-}
+const App = () => {
+    const initialState = {
+        name: "Guster",
+        activity: "purring"
+    };
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "changeActivity":
+                return {
+                    ...state,
+                    activity: action.activity
+                };
+            case "changeName":
+                return {
+                    ...state,
+                    name: action.newName
+                };
+            default:
+                return state;
+        }
+    };
+    
+    return (
+        <div className="App">
+            <StateProvider value={useReducer(reducer, initialState)}>
+                <Activity />
+            </StateProvider>
+        </div>
+    );
+};
 
 export default App;
